eba727fcd891cbf194dc1dbdcb68cd664d2f42c8
Julius <123553495+Julius0087@users.noreply.github.com>
2023-05-20 12:16:09 +0200

    Updated version
    - add a rainbow mode - generates a random rgb values for each pixel
    - add a button to toggle the rainbow mode
    - add the rainbow mode button CSS
    - add the "switch" functionality to the rainbow mode button

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 
 const containerDiv = document.querySelector(".canvas-container");
 const gridButton = document.querySelector("#grid-selection");
+const rainbowButton = document.querySelector(".rainbow-checkbox");
 
 let color = "gray";
 let size = 16;
 const canvasWidth = 800;
+
+let rainbowMode = false;
 
 function renderGrid(size) {
   const pixelSize = canvasWidth / size;
@@ -24,6 +27,7 @@ renderGrid(size);
 
 
 gridButton.addEventListener("click", gridPrompt);
+rainbowButton.addEventListener("click", rainbowSwitch);
 
 function gridPrompt() {
   size = prompt("Select the size of the grid (1-80)");
@@ -38,12 +42,35 @@ function gridPrompt() {
   renderGrid(size);
 }
 
-function hover(e) {
-  e.target.style.backgroundColor = color;
+function rainbowSwitch() {
+  if (rainbowMode) {
+    rainbowMode = false;
+    rainbowButton.classList.toggle("active-rainbow");
+  }
+  else {
+    rainbowMode = true;
+    rainbowButton.classList.toggle("active-rainbow");
+  }
+  console.log(rainbowMode)
 }
 
+function hover(e) {
+  console.log(rainbowMode)
+  if (rainbowMode === true) {
+    e.target.style.backgroundColor = rainbowColor();
+  }
+  else {
+    e.target.style.backgroundColor = color;
+  }
+  
+}
+
+function rainbowColor() {
+  let randomRGB = `rgb(${Math.floor(Math.random() * (255 - 0 + 1) + 0)}, ${Math.floor(Math.random() * (255 - 0 + 1) + 0)}, ${Math.floor(Math.random() * (255 - 0 + 1) + 0)})`;
+    return randomRGB;
+}
+
+
 // extra TODO:
-/* add the rainbow functionality
-try to change the listener for click and hold
-color changer */
+/* color changer */
 
