6c89caa45b2760113471fe79b81b8b06b1bd8da3
Julius <123553495+Julius0087@users.noreply.github.com>
2023-05-21 12:22:19 +0200

    Updated version
    - add clear grid button
    - add clear grid button functionality
    - add code refactor - remove and render grid into separate functions
    - add shading button
    - add shading button functionality - on rainbow mode
    - add shading button functionality - limited

--- script.js
+++ script.js
@@ -2,13 +2,24 @@
 
 const containerDiv = document.querySelector(".canvas-container");
 const gridButton = document.querySelector("#grid-selection");
-const rainbowButton = document.querySelector(".rainbow-checkbox");
+const clearButton = document.querySelector(".clear-grid");
 
-let color = "gray";
+const rainbowButton = document.querySelector(".rainbow-checkbox");
+const shadingButton = document.querySelector(".shading-checkbox");
+
+let color = "rgba(0, 0, 0, 1)";
 let size = 16;
 const canvasWidth = 800;
 
 let rainbowMode = false;
+let shadingMode = false;
+
+function removeGrid() {
+  const divs = document.querySelectorAll(".pixel");
+  for (let i = 0; i < divs.length; i++) {
+    containerDiv.removeChild(divs[i]);
+  }
+}
 
 function renderGrid(size) {
   const pixelSize = canvasWidth / size;
@@ -27,18 +38,19 @@ renderGrid(size);
 
 
 gridButton.addEventListener("click", gridPrompt);
+clearButton.addEventListener("click", () => {
+  removeGrid();
+  renderGrid(size);
+});
 rainbowButton.addEventListener("click", rainbowSwitch);
+shadingButton.addEventListener("click", shadingSwitch);
 
 function gridPrompt() {
   size = prompt("Select the size of the grid (1-80)");
   while (size > 80 || size < 0) {
     size = prompt("Please select a number between 1 and 80");
   }
-
-  const divs = document.querySelectorAll(".pixel");
-  for (let i = 0; i < divs.length; i++) {
-    containerDiv.removeChild(divs[i]);
-  }
+  removeGrid();
   renderGrid(size);
 }
 
@@ -54,23 +66,51 @@ function rainbowSwitch() {
   console.log(rainbowMode)
 }
 
+function shadingSwitch() {
+  if (shadingMode) {
+    shadingMode = false;
+    shadingButton.classList.toggle("active-shading");
+  }
+  else {
+    shadingMode = true;
+    shadingButton.classList.toggle("active-shading");
+  }
+}
+
 function hover(e) {
-  console.log(rainbowMode)
-  if (rainbowMode === true) {
+  /* console.log(rainbowMode) */
+  if (shadingMode === true && rainbowMode === true) {
+    e.target.style.backgroundColor = rainbowColor(0.1);
+  }
+  else if (rainbowMode === true) {
     e.target.style.backgroundColor = rainbowColor();
   }
+  else if (shadingMode === true) {
+    console.log("shading");
+    e.target.style.backgroundColor = changeColor(0.1);
+  }
+  
   else {
     e.target.style.backgroundColor = color;
   }
-  
 }
 
-function rainbowColor() {
-  let randomRGB = `rgb(${Math.floor(Math.random() * (255 - 0 + 1) + 0)}, ${Math.floor(Math.random() * (255 - 0 + 1) + 0)}, ${Math.floor(Math.random() * (255 - 0 + 1) + 0)})`;
-    return randomRGB;
+function changeColor(opacity = 1) {
+  return `rgba(0, 0, 0, ${opacity})`;
+}
+
+function rainbowColor(opacity = 1) {
+  let randomRGB = `rgba(${Math.floor(Math.random() * (255 - 0 + 1) + 0)}, ${Math.floor(Math.random() * (255 - 0 + 1) + 0)}, ${Math.floor(Math.random() * (255 - 0 + 1) + 0)}, ${opacity})`; 
+  return randomRGB;
+}
+
+function shadingColor() {
+  return color;
 }
 
 
 // extra TODO:
-/* color changer */
+/* color changer
+clear grid
+shading */
 
